d62777a62b955408c1c796146f871726f54b0342
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-16 13:34:10 -0600

    Started project for class

---
+++ SquarePyramid.py
@@ -0,0 +1,6 @@
+# MartinezP1
+# Programmer: Adelita Martinez
+# Email: amartinez1013@cnm.edu
+# Purpose: Make a program to calculate the surgace area and volume of a square pyramid
+# Volume of a pyramid
+# Python Version:
