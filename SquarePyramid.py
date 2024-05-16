b7af2a8083833d2f5cc529d6dffd2e109219769d
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-16 14:08:24 -0600

    Added formulas

--- SquarePyramid.py
+++ SquarePyramid.py
@@ -4,3 +4,22 @@
 # Purpose: Make a program to calculate the surgace area and volume of a square pyramid
 # Volume of a pyramid
 # Python Version:
+
+# Display a header explaing the program
+
+# Get the length of the base from user
+
+# Get the height of the pyramid from user
+
+# Calculate the volume
+
+# Calculate the slant height
+
+# Calculate the area of one pyramid side
+
+# Calculate the surface area of the four sides
+
+# Formula: 
+# volume = a**2h / 3
+# slantheight = sqrt(h**2 + (a / 2)**2)
+# Area of one pyramid side = slantheight * a/2
